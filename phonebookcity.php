0caf0f0094576ad478064406fcb74c0acbbae7e4
yesalam <itisalam@gmail.com>
2017-07-14 02:07:40 +0530

    fetch city to show autocompleteTextView

---
+++ phonebookcity.php
@@ -0,0 +1,20 @@
+<?php
+               //$connect=mysqli_connect("localhost","","","","");
+$connect=mysqli_connect("localhost","simptnhu","+d2n4?%KwE7!","simptnhu_yomarket","3306");
+
+          $sql = "SELECT DISTINCT phonebook_city FROM `phonebook`" ;
+                    //echo $sql;
+
+                    $result = mysqli_query($connect, $sql);
+
+
+
+         			$json_array = array();
+         			while($row = mysqli_fetch_assoc($result))
+                    {
+                         $json_array[] = $row;
+                    }
+
+                    echo json_encode(array("server response"=>$json_array));
+         		   mysqli_close($connect);
+?>
