6c0641bccf3ffa8f33f115b86003daacef26c6dd
yesalam <itisalam@gmail.com>
2017-08-17 22:11:22 +0530

    forgotPassword added

---
+++ forgotPassword.php
@@ -0,0 +1,26 @@
+<?php
+$connect=mysqli_connect("localhost","simptnhu","+d2n4?%KwE7!","simptnhu_yomarket","3306");
+
+$id = $_GET['userid'];
+//$password=$_GET['password'];
+
+//$sql = "SELECT * FROM register2 where register_email_address='$id' and register_password='$password';";
+
+
+					$sql = "SELECT * FROM `register2` where register_email_address='$id'" ;
+										//echo $sql;
+
+										$result = mysqli_query($connect, $sql);
+
+
+
+							$json_array = array();
+							while($row = mysqli_fetch_assoc($result))
+										{
+												 $json_array[] = $row;
+										}
+
+										echo json_encode(array("server response"=>$json_array));
+							 mysqli_close($connect);
+
+?>
